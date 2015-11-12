e140f500da0620148e8629e86a32934da9d01f0e
wshino <wshinohara@gmail.com>
2015-11-13 00:03:29 +0900

    Update and rename sql.md to sql.sql

--- sql.sql
+++ sql.sql
@@ -1,34 +1,33 @@
+-- Aggregation
 
 
+-- Revising Aggregations - The Count Function
 
-## Aggregation
-
-### Revising Aggregations - The Count Function
-
-```Oracle
+-- Oracle
 SELECT COUNT(ID) FROM City WHERE Population > 100000;
-```
 
-### Revising Aggregations - The Sum function
 
-```Oracle
+-- Revising Aggregations - The Sum function
+
+-- Oracle
 SELECT SUM(Population) FROM City WHERE District = 'California';
-```
 
-### Revising Aggregations - Averages
 
-```Oracle
+-- Revising Aggregations - Averages
+
+-- Oracle
 SELECT AVG(Population) FROM City WHERE District = 'California';
-```
 
-### Average Population
 
-```MySQL
+-- Average Population
+
+-- MySQL
 SELECT ROUND(AVG(Population)) FROM City;
-```
 
-### Japan Population
 
-```MySQL
+-- Japan Population
+
+-- MySQL
 SELECT SUM(Population) FROM City WHERE CountryCode = 'JPN';
-```
+
+
